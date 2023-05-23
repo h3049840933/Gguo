af743a1a8862d6f712bc4380c98d3a4aecd9616f
unknown <3049840933@qq.com>
2023-05-23 09:30:47 +0800

    hmy0523 注册模块完成

--- js/regist.js
+++ js/regist.js
@@ -15,3 +15,29 @@ code_.onclick = function () {
         }
     }, 1000)
 }
+
+var btn = document.getElementById('sumbit');
+var tel = document.querySelector('.tel');
+var imgCode = document.querySelector('.imgCode');
+var psw = document.querySelector('.password');
+var rePsw = document.querySelector('.rePsw');
+var ipt = document.getElementsByTagName('input');
+
+btn.onclick = function () {
+    if (/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/.test(tel.value)) {
+        if (imgCode.value == 'r2b7') {
+            if (psw.value == rePsw.value) {
+                window.location.href = "index.html";
+                for (var i in ipt) {
+                    ipt[i].value = '';
+                }
+            } else {
+                alert('两次输入的密码不一样')
+            }
+        } else {
+            alert('图片验证码错误')
+        }
+    } else {
+        alert('请输入正确的手机号')
+    }
+}
