a3480b973d81e41c50eaebabb83df07f6d552ec2
unknown <3049840933@qq.com>
2023-05-24 11:33:00 +0800

    hmyEnd

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-var str = '';
-var data = [];
-window.onload = function () {
-    var ajax = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
-=======
 
 var mySwiper = new Swiper('.swiper', {
     direction: 'horizontal', // 垂直切换选项
@@ -25,51 +19,12 @@ var mySwiper = new Swiper('.swiper', {
 var data;
 (function () {
     let ajax = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
->>>>>>> 5fa3edf28e1c344acb836c331ccc2c12fe58f724
     ajax.open('get', 'http://127.0.0.1:3000/useing/public');
     ajax.send();
     ajax.onreadystatechange = function () {
         if (ajax.readyState == 4) {
             if (ajax.status == 200) {
                 data = JSON.parse(ajax.responseText);
-<<<<<<< HEAD
-                console.log(data);
-                showData();
-            }
-            else {
-                console.log('加载失败');
-            }
-        }
-    }
-}
-
-
-function showData() {
-    var shuju = document.getElementsByTagName('dl')[0];
-    for (var item of data) {
-        str = `<dt>
-        <a href="#">
-        <div class="shoufa"><a>${item.info_ty}</a></div>
-        <img src=${item.img} width="220" height="130">
-        <div class="hot-con">
-           <h2 class="name">${item.text}</h2>
-           <p class="tabs">
-                <span><a>${item.totalnum}</a></span>
-                <span><a>${item.num}</a></span>
-           </p>
-           <p class="sq">
-                <span>${item.apply}</span>申请
-           </p>
-           <p class="current">报告数量:8</p>
-        </div>
-        </a>
-        </dt>`
-        console.log(str);
-        shuju.innerHTML += str;
-    }
-}
-showData();
-=======
             }
         }
     }
@@ -151,4 +106,3 @@ function showData() {
 
 setTimeout(showData, 500);
 
->>>>>>> 5fa3edf28e1c344acb836c331ccc2c12fe58f724
